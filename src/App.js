a18171eea5f8efb0606ca5419a6dcd723afc08c4
marvedly <3909573+marvedly@users.noreply.github.com>
2021-10-14 19:01:52 +0200

    Did some changes to App.js without considering testing

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://consid.se/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Consid
         </a>
       </header>
     </div>
