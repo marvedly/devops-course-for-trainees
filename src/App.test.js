09995126d983d3aa071d4a503da79bd3b0a10fd6
marvedly <3909573+marvedly@users.noreply.github.com>
2021-10-14 19:04:43 +0200

    Fixed tests, oops!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Consid link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/consid/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('link leads to Consid', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/consid/i);
+  expect(linkElement.closest('a')).toHaveAttribute('href', 'https://consid.se/')
 });
